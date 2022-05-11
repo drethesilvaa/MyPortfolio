86f5464b26cecaa156eb3a74c402adce12762efa
drethesilva <andressilva03@gmail.com>
2022-05-11 22:27:21 +0100

    Navbar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,9 @@ import { SocialIcon } from "react-social-icons";
 import { HashLink as Link } from "react-router-hash-link";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { AnimatedOnScroll } from "react-animated-css-onscroll";
+import { useMediaQuery } from "react-responsive";
+
+import "../stylesheets/navbar.css";
 
 require("bootstrap");
 
@@ -11,6 +14,8 @@ export default function NavBar() {
   const stylenav =
     "inline-flex items-center lg:py-3 px-3 lg:my-6 rounded text-green-100 hover:text-white tracking-wide text-2xl";
 
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 991px)" });
+  const isBigScreen = useMediaQuery({ query: "(min-width: 992px)" });
   // const activeStyleNav = "text-red-1999 bg-red-700";
 
   const socialIconsStyle = { height: 35, width: 35 };
@@ -19,7 +24,11 @@ export default function NavBar() {
     <header className="">
       <Navbar bg="transparent" variant="dark" expand="lg">
         <Container>
-          <AnimatedOnScroll animationIn="fadeIn" animationInDelay={500}>
+          <AnimatedOnScroll
+            animationIn="fadeIn"
+            animationInDelay={500}
+            screenOffset={0}
+          >
             <Navbar.Brand href="#home">
               <Link
                 to="/"
@@ -30,48 +39,84 @@ export default function NavBar() {
               </Link>
             </Navbar.Brand>
           </AnimatedOnScroll>
-
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              {/* <Nav.Link>
-                {" "}
-                <NavLink to="/post" className={stylenav}>
-                  Blog Posts
-                </NavLink> 
-              </Nav.Link>*/}
-              <AnimatedOnScroll animationIn="fadeIn" animationInDelay={1000}>
-                <Nav.Link>
-                  <Link to="/#myprojects" smooth className={stylenav}>
-                    Projects
-                  </Link>
-                </Nav.Link>
-              </AnimatedOnScroll>
-              <AnimatedOnScroll animationIn="fadeIn" animationInDelay={1250}>
-                <Nav.Link>
-                  <Link to="/#aboutme" smooth className={stylenav}>
-                    About Me
-                  </Link>
-                </Nav.Link>
-              </AnimatedOnScroll>
-              <AnimatedOnScroll animationIn="fadeIn" animationInDelay={1500}>
-                <div className="inline-flex lg:py-3 px-3 lg:my-6">
-                  <SocialIcon
-                    url="https://github.com/drethesilvaa"
-                    className="mr-4"
-                    target={"_blank"}
-                    fgColor="#fff"
-                    style={socialIconsStyle}
-                  ></SocialIcon>
-                  <SocialIcon
-                    url="https://www.linkedin.com/in/andréssilva/"
-                    className="mr-4"
-                    target={"_blank"}
-                    fgColor="#fff"
-                    style={socialIconsStyle}
-                  ></SocialIcon>
-                </div>
-              </AnimatedOnScroll>
+              {isTabletOrMobile && (
+                <React.Fragment>
+                  <Nav.Link>
+                    <Link to="/#myprojects" smooth className={stylenav}>
+                      Projects
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to="/#aboutme" smooth className={stylenav}>
+                      About Me
+                    </Link>
+                  </Nav.Link>
+                  <div className="inline-flex lg:py-3 px-3 lg:my-6">
+                    <SocialIcon
+                      url="https://github.com/drethesilvaa"
+                      className="mr-4"
+                      target={"_blank"}
+                      fgColor="#fff"
+                      style={socialIconsStyle}
+                    ></SocialIcon>
+                    <SocialIcon
+                      url="https://www.linkedin.com/in/andréssilva/"
+                      className="mr-4"
+                      target={"_blank"}
+                      fgColor="#fff"
+                      style={socialIconsStyle}
+                    ></SocialIcon>
+                  </div>
+                </React.Fragment>
+              )}
+              {isBigScreen && (
+                <React.Fragment>
+                  <AnimatedOnScroll
+                    animationIn="fadeIn"
+                    animationInDelay={1000}
+                  >
+                    <Nav.Link>
+                      <Link to="/#myprojects" smooth className={stylenav}>
+                        Projects
+                      </Link>
+                    </Nav.Link>
+                  </AnimatedOnScroll>
+                  <AnimatedOnScroll
+                    animationIn="fadeIn"
+                    animationInDelay={1250}
+                  >
+                    <Nav.Link>
+                      <Link to="/#aboutme" smooth className={stylenav}>
+                        About Me
+                      </Link>
+                    </Nav.Link>
+                  </AnimatedOnScroll>
+                  <AnimatedOnScroll
+                    animationIn="fadeIn"
+                    animationInDelay={1500}
+                  >
+                    <div className="inline-flex lg:py-3 px-3 lg:my-6">
+                      <SocialIcon
+                        url="https://github.com/drethesilvaa"
+                        className="mr-4"
+                        target={"_blank"}
+                        fgColor="#fff"
+                        style={socialIconsStyle}
+                      ></SocialIcon>
+                      <SocialIcon
+                        url="https://www.linkedin.com/in/andréssilva/"
+                        className="mr-4"
+                        target={"_blank"}
+                        fgColor="#fff"
+                        style={socialIconsStyle}
+                      ></SocialIcon>
+                    </div>
+                  </AnimatedOnScroll>
+                </React.Fragment>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
